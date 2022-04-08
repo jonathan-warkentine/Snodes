54972259c0e3f47b3f0041e745a34ed2bdfe4fcc
Mitchel Wachtel <mitchel.wachtel@gmail.com>
2022-04-08 19:59:34 -0400

    added route for homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,12 +1,35 @@
 const router = require("express").Router();
-const {User} = require("../models");
+const {User, Codesnip, Favorite, Tag, Codesniptag} = require("../models");
 const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
   try {
-
+    if(req.session.logged_in) {
+      const snodeData = await Favorite.findAll({
+        where: {
+          user_id: req.session.user_id,
+        },
+        include: [
+          {
+            model: User,
+            attributes: ["name"],
+          },
+        ],
+    })} else {
+      // TODO: Build this to make the snodeData only the top favorited snodes
+      const snodeData = await Codesnip.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ["name"],
+          },
+        ],
+    })
+    }
+    const snodes = snodeData.map((snode) => snode.get({plain: true}));
     // Pass serialized data and session flag into template
     res.render("homepage", {
+      snodes,
       logged_in: req.session.logged_in 
     });
     // res.json(blogpost);
