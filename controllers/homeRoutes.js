e8bcd1f087075796e2137fcb464a0349741f3ca6
Mitchel Wachtel <mitchel.wachtel@gmail.com>
2022-04-08 20:11:06 -0400

    added route to go to draft a new snode /draftsnode

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,10 +2,12 @@ const router = require("express").Router();
 const {User, Codesnip, Favorite, Tag, Codesniptag} = require("../models");
 const withAuth = require("../utils/auth");
 
+// If logged in, displays the user's favorites on homepage, if not logged in, displays top snodes
 router.get("/", async (req, res) => {
   try {
-    if(req.session.logged_in) {
-      const snodeData = await Favorite.findAll({
+    let snodeData;
+    if (req.session.logged_in) {
+      snodeData = await Favorite.findAll({
         where: {
           user_id: req.session.user_id,
         },
@@ -14,23 +16,70 @@ router.get("/", async (req, res) => {
             model: User,
             attributes: ["name"],
           },
+          {
+            model: Codesnip,
+          },
         ],
-    })} else {
+      });
+    } else {
       // TODO: Build this to make the snodeData only the top favorited snodes
-      const snodeData = await Codesnip.findAll({
+      snodeData = await Codesnip.findAll({
         include: [
           {
             model: User,
             attributes: ["name"],
           },
         ],
-    })
+      });
     }
     const snodes = snodeData.map((snode) => snode.get({plain: true}));
     // Pass serialized data and session flag into template
     res.render("homepage", {
       snodes,
-      logged_in: req.session.logged_in 
+      logged_in: req.session.logged_in,
+    });
+    // res.json(blogpost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Gets the current user's favorited snodes and pushes them to favsnodes.handlebars
+router.get("/favsnodes", withAuth, async (req, res) => {
+  try {
+    snodeData = await Favorite.findAll({
+      where: {
+        user_id: req.session.user_id,
+      },
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+        {
+          model: Codesnip,
+        },
+      ],
+    });
+
+    const snodes = snodeData.map((snode) => snode.get({plain: true}));
+
+    // Pass serialized data and session flag into template
+    res.render("favsnodes", {
+      snodes,
+      logged_in: req.session.logged_in,
+    });
+    // res.json(blogpost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Sends user to the page to draft up a new snode
+router.get("/draftsnode", withAuth, async (req, res) => {
+  try {
+    res.render("draftsnode", {
+      logged_in: req.session.logged_in,
     });
     // res.json(blogpost);
   } catch (err) {
@@ -47,6 +96,5 @@ router.get("/login", (req, res) => {
 
   res.render("login");
 });
-
 
 module.exports = router;
