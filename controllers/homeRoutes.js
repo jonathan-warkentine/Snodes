4e7c059b6da659fde21cc8a8f571af82028c67b6
Mitchel Wachtel <mitchel.wachtel@gmail.com>
2022-04-10 15:16:30 -0400

    working algorithm to get json for a user's favorite codesnips

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
-const {User, Codesnip, Favorite, Tag, Codesniptag} = require("../models");
+const {User, Codesnip, Tag, Codesniptag} = require("../models");
 const withAuth = require("../utils/auth");
+const {Op} = require("sequelize");
 
 // If logged in, displays the user's favorites on homepage, if not logged in, displays top snodes
 router.get("/", async (req, res) => {
@@ -97,20 +98,22 @@ router.get("/profile/:id", withAuth, async (req, res) => {
       ],
     });
 
-      personalSnodeData = await Codesnip.findAll({
-        where: {
-          user_id: req.params.id,
+    personalSnodeData = await Codesnip.findAll({
+      where: {
+        user_id: req.params.id,
+      },
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
         },
-        include: [
-          {
-            model: User,
-            attributes: ["name"],
-          },
-        ],
-      });
+      ],
+    });
 
     const favSnodes = favSnodeData.map((snode) => snode.get({plain: true}));
-    const personalSnodes = personalSnodeData.map((snode) => snode.get({plain: true}));
+    const personalSnodes = personalSnodeData.map((snode) =>
+      snode.get({plain: true})
+    );
 
     // Pass serialized data and session flag into template
     res.render("profile", {
@@ -136,8 +139,6 @@ router.get("/draftsnode", withAuth, async (req, res) => {
   }
 });
 
-
-
 router.get("/login", (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
@@ -146,6 +147,52 @@ router.get("/login", (req, res) => {
   }
 
   res.render("login");
+});
+
+//
+// TESTING: NO CHANGES WERE MADE ABOVE THIS
+// Except: Favorites was removed from imports
+//
+router.get("/testing/:id", async (req, res) => {
+  try {
+    // snodeData = await User.findByPk(req.params.id);
+
+    allData = await User.findAll({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    const allData2 = allData.map((user) => user.get({plain: true}));
+
+    const favString = allData2[0].favorites;
+    const favArrStrings = favString.split(",");
+    const favArr = favArrStrings.map((element) => parseInt(element));
+
+    const arrFavObj = favArr.reduce(function (acc, favId) {
+      return [...acc, {id: favId}];
+    }, []);
+
+    favSnodeData = await Codesnip.findAll(
+      {
+      where: {
+        [Op.or]: arrFavObj,
+      },
+    }
+    );
+
+    const favSnodes = favSnodeData.map((snode) => snode.get({plain: true}));
+
+    // // Pass serialized data and session flag into template
+    // res.render("favsnodes", {
+    //   snodes,
+    //   logged_in: req.session.logged_in,
+    // });
+
+    res.json(favSnodes);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
